b16a07e2f8575f686847a8c79f6befd2bf2b61de
kviswas1216 <kviswas1216@gmail.com>
2025-03-27 16:28:59 +0530

    Error.ejs

--- app.js
+++ app.js
@@ -127,13 +127,9 @@ app.all("*",(req,res,next)=>{
 
 app.use((err,req,res,next)=>{
   let {statusCode=500,message="Some thing went wrong!"}=err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs",{ message });
+  // res.status(statusCode).send(message);
 })
-
-
-
-
-
 
 
 
