9cfb6d07feee373dca1c30a41d6223ba19dee957
kviswas1216 <kviswas1216@gmail.com>
2025-04-02 09:55:26 +0530

    Phase 2 part b - Express Router

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Listing = require("./Models/listing.js"); 
 const path = require("path");
-const methodOverride= require("method-override");
+const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapasync = require("./utils/wrapasync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema,reviewSchema} = require("./schema.js");
+const { reviewSchema } = require("./schema.js");
 const Review = require("./Models/review.js");
-
+const listings = require("./routes/listing.js"); // Fixed path
+const Listing = require("./Models/listing.js");
+ 
 async function main() {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/Stayscape");
@@ -21,21 +22,6 @@ async function main() {
 main();
 
 const app = express();
-
-
-//validation Listing
-const validateListing = (req, res, next) => {
-  if (!req.body || !req.body.listing) {
-    throw new ExpressError(400, "Invalid listing data!");
-  }
-
-  let { error } = listingSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  }
-  next();
-};
 
 
 //validate Review
@@ -51,6 +37,8 @@ const validateReview = (req, res, next) => {
   }
   next();
 };
+
+app.use("/listings",listings);
 
 
 //path
@@ -86,73 +74,6 @@ app.get("/testListing", async (req, res) => {
   res.send("successful testing");
 
 });
-
-
-//index route retrieve listings
-app.get("/listings",wrapasync(async (req,res)=>{
-
-  const allListings= await Listing.find({});
-  res.render("listings/index.ejs",{allListings});
-
-}));
-
-
-//New listing route form
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
-});//this shoudl be above show route code
-//because if it overwise it thinks new as id
-
-//create Route
-app.post("/listings",validateListing,wrapasync(async (req,res,next)=>{
-    // in form name=title then below
-    // let {title,description,price,image,country,location}=req.body;
-    //in form name=listing[title] array then this
-    // client didnot send valid request then
-
-    const newlisting= new Listing(req.body.listing);
-    await newlisting.save()
-    res.redirect("/listings");
-
-  })
-);
-
-//Show Route
-app.get("/listings/:id",wrapasync(async(req,res)=>{
-
-  let {id}=req.params;
-  const listing=await Listing.findById(id).populate("reviews");
-  res.render("listings/show.ejs",{listing});
-
-}));
-
-//edit Route
-app.get("/listings/:id/edit",wrapasync(async (req,res)=>{
-
-  let {id}=req.params;
-  const listing=await Listing.findById(id);
-  res.render("listings/edit.ejs",{listing});
-
-}));
-
-//update
-app.put("/listings/:id",validateListing, wrapasync(async (req,res) =>{
-  
-  let {id}=req.params;
-  await Listing.findByIdAndUpdate(id,{...req.body.listing});
-  res.redirect(`/listings/${id}`);
-  
-}));
-
-//Delete
-app.delete("/listings/:id", wrapasync(async (req,res) =>{
-  
-    let {id}=req.params;
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-   
-}));
-
 
 // Add Route for Reviews
 app.post("/listings/:id/reviews", validateReview,async(req,res)=>{
---
+++ routes/listing.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const wrapasync = require("../utils/wrapasync.js");
+const { listingSchema } = require("../schema.js");
+const ExpressError = require("../utils/ExpressError.js");
+const Listing = require("../Models/listing.js");
+
+//validation Listing
+const validateListing = (req, res, next) => {
+    if (!req.body || !req.body.listing) {
+      throw new ExpressError(400, "Invalid listing data!");
+    }
+  
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+      let errMsg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(400, errMsg);
+    }
+    next();
+};
+
+//index route retrieve listings
+router.get("/",wrapasync(async (req,res)=>{
+
+  const allListings= await Listing.find({});
+  res.render("listings/index.ejs",{allListings});
+
+}));
+
+//New listing route form
+router.get("/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});//this shoudl be above show route code
+//because if it overwise it thinks new as id
+
+//create Route
+router.post("/listings",validateListing,wrapasync(async (req,res,next)=>{
+    // in form name=title then below
+    // let {title,description,price,image,country,location}=req.body;
+    //in form name=listing[title] array then this
+    // client didnot send valid request then
+
+    const newlisting= new Listing(req.body.listing);
+    await newlisting.save()
+    res.redirect("/listings");
+
+  })
+);
+
+//Show Route
+router.get("/:id",wrapasync(async(req,res)=>{
+
+  let {id}=req.params;
+  const listing=await Listing.findById(id).populate("reviews");
+  res.render("listings/show.ejs",{listing});
+
+}));
+
+//edit Route
+router.get("/:id/edit",wrapasync(async (req,res)=>{
+
+  let {id}=req.params;
+  const listing=await Listing.findById(id);
+  res.render("listings/edit.ejs",{listing});
+
+}));
+
+//update
+router.put("/:id",validateListing, wrapasync(async (req,res) =>{
+  
+  let {id}=req.params;
+  await Listing.findByIdAndUpdate(id,{...req.body.listing});
+  res.redirect(`/listings/${id}`);
+  
+}));
+
+//Delete
+router.delete("/:id", wrapasync(async (req,res) =>{
+  
+    let {id}=req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+   
+}));
+
+
+module.exports=router;
