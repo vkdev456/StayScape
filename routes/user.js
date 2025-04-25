bd45d7a75bff0f5670c7fd2026dcd122909f998e
kviswas1216 <kviswas1216@gmail.com>
2025-04-25 20:42:03 +0530

    login

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User=require("../models/user.js");  
 const wrapasync = require("../utils/wrapasync.js");
+const passport=require("passport");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
@@ -19,8 +20,17 @@ router.post("/signup",wrapasync(async(req,res)=>{
        req.flash("error",e.message);  
        res.redirect("/signup");   
    }
-
 }));
 
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+})
+ 
+router.post("/login",passport.authenticate("local",{failureRedirect:'/login',failureFlash:true}),
+    async(req,res)=>{
+      res.flash("success","welcome to StayScape! You are Logged in!");
+      res.redirect("/listings");
+    }
+);
 module.exports=router;
 
