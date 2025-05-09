83f3370ef4189d089205581bb69aa486938610d5
kviswas1216 <kviswas1216@gmail.com>
2025-05-09 13:53:54 +0530

    Deployment Correction

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const MongoStore=require("connect-mongo");
 const flash=require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local");
-const User=require("../models/user.js");
+const User=require("./models/user.js");
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
